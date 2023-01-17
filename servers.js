a310c68b2630cbc56bd03e1d960df8ff7bee2a97
AtibProgrammer <atibkhan392@gmail.com>
2023-01-18 03:17:26 +0530

    Edit Button added Successfully

--- servers.js
+++ servers.js
@@ -11,9 +11,19 @@ function onSubmit(e) {
     btn.value='Delete';
     btn.type='button'
     btn.appendChild(document.createTextNode('delete'))
+
+    const edit=document.createElement('input')
+    edit.value='Edit';
+    edit.type='button'
+    edit.appendChild(document.createTextNode('edit'))
+    
+
+    
     li.appendChild(document.createTextNode(`${nameInput.value}-${emailInput.value}-${phoneInput.value}`));
     userList.appendChild(li);
     li.appendChild(btn);
+    li.appendChild(edit);
+    
 
     let myObject={
       name:nameInput.value,
@@ -32,7 +42,7 @@ function onSubmit(e) {
   var itemList=document.getElementById('users')
   itemList.addEventListener('click',removeItem)
   function removeItem(e){
-    if(e.target.type==='button'){
+    if(e.target.value==='Delete'){
         if(confirm('Are you sure?')){
             var li=e.target.parentElement;
             a=li.textContent
@@ -40,6 +50,17 @@ function onSubmit(e) {
             localStorage.removeItem(myArray[1]);
             itemList.removeChild(li)
         }
+    }
+    else if(e.target.value==='Edit'){
+      var li=e.target.parentElement;
+            a=li.textContent
+            const myArray=a.split('-')
+            nameInput.value = myArray[0];
+            emailInput.value = myArray[1];
+            phoneInput.value = myArray[2];
+            localStorage.removeItem(myArray[1]);
+            itemList.removeChild(li)
+
     }
 }
 
